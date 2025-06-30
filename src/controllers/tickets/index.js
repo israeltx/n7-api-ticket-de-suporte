ebbd4f1735f1f1ef2ec17886245d342010d502fa
Israel Teixeira <israeltx@gmail.com>
2025-06-30 17:45:15 -0300

    Add dynamic parameters for filtering

--- src/controllers/tickets/index.js
+++ src/controllers/tickets/index.js
@@ -8,6 +8,8 @@
  */
 
 export function index({request, response, database}) {
+  const {status} = request.query
   const tickets = database.select('tickets')
+
   return response.end(JSON.stringify(tickets))
 }
